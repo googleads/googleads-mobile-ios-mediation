72f5b0608a86fb210f7469b7127d5077473398f8
Andrew Brogdon <brogdon@gmail.com>
2017-05-11 14:19:08 -0700

    Adds FAN, ONE by AOL, and some fixes.

---
+++ adapters/Facebook/CHANGELOG.md
@@ -0,0 +1,52 @@
+# Facebook Audience Network Mediation Adapter for Google Mobile Ads SDK for iOS
+
+## Version 4.22.0.0
+- Verified compatibility with FAN SDK 4.22.0.
+
+## Version 4.21.0.0
+- Verified compatibility with FAN SDK 4.21.0.
+
+## Version 4.20.2.0
+- Verified compatibility with FAN SDK 4.20.2.
+
+## Version 4.20.1.0
+- Verified compatibility with FAN SDK 4.20.1.
+
+## Version 4.20.0.0
+- Verified compatibility with FAN SDK 4.20.0.
+
+## Version 4.19.0.0
+- Verified compatibility with FAN SDK 4.19.0.
+
+## Version 4.18.0.0
+- Verified compatibility with FAN SDK 4.18.0.
+- Fixed a bug for native ads where AdChoices icon was rendering out of bounds.
+
+## Version 4.17.0.0
+- Changed the version naming system to
+  [FAN SDK version].[adapter patch version].
+- Updated the minimum required Google Mobile Ads SDK to v7.12.0.
+- Added support for native ads.
+
+## Version 1.4.0
+- Requires Google Mobile Ads SDK 7.8.0 or higher.
+- Requires FAN SDK 4.13.1 or higher.
+- Adapter now uses [kFBAdSizeInterstitial] instead of [kFBAdSizeInterstital].
+- Enabled bitcode.
+
+## Version 1.2.1
+- Fixed a bug where interstitial presented and interstitial leaving application
+  callbacks were not properly invoked.
+
+## Version 1.2.0
+- Sends callbacks when an ad is presented and dismissed.
+
+## Version 1.1.0
+- Added support for full width x 250 format when request is
+  for `kGADAdSizeMediumRectangle`.
+
+## Version 1.0.1
+- Added support for `kGADAdSizeSmartBanner`.
+
+## Version 1.0.0
+- Initial release.
---
+++ adapters/Facebook/FacebookAdapter/GADMAdapterFacebook.m
@@ -0,0 +1,116 @@
+// Copyright 2016 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "GADMAdapterFacebook.h"
+
+@import FBAudienceNetwork;
+
+#import "GADFBBannerAd.h"
+#import "GADFBError.h"
+#import "GADFBInterstitialAd.h"
+#import "GADFBNativeAd.h"
+#import "GADFBNetworkExtras.h"
+
+@interface GADMAdapterFacebook () {
+  /// Connector from Google Mobile Ads SDK to receive ad configurations.
+  __weak id<GADMAdNetworkConnector> _connector;
+  /// Facebook Audience Network banner ad wrapper.
+  GADFBBannerAd *_bannerAd;
+  /// Facebook Audience Network interstitial ad wrapper.
+  GADFBInterstitialAd *_interstitialAd;
+  /// Facebook Audience Network native ad wrapper.
+  GADFBNativeAd *_nativeAd;
+}
+@end
+
+@implementation GADMAdapterFacebook
+
++ (NSString *)adapterVersion {
+  return @"4.22.0.0";
+}
+
++ (Class<GADAdNetworkExtras>)networkExtrasClass {
+  return [GADFBNetworkExtras class];
+}
+
+- (instancetype)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)connector {
+  if (![[GADMobileAds sharedInstance] isSDKVersionAtLeastMajor:7 minor:12 patch:0]) {
+    NSLog(@"Unsupported SDK. GoogleMobileAds SDK version 7.12.0 or higher is required.");
+    return nil;
+  }
+
+  self = [self init];
+  if (self) {
+    _bannerAd = [[GADFBBannerAd alloc] initWithGADMAdNetworkConnector:connector adapter:self];
+    _interstitialAd =
+        [[GADFBInterstitialAd alloc] initWithGADMAdNetworkConnector:connector adapter:self];
+    _nativeAd = [[GADFBNativeAd alloc] initWithGADMAdNetworkConnector:connector adapter:self];
+    _connector = connector;
+  }
+  return self;
+}
+
+- (void)getBannerWithSize:(GADAdSize)adSize {
+  id<GADMAdNetworkConnector> strongConnector = _connector;
+  if ([strongConnector respondsToSelector:@selector(childDirectedTreatment)] &&
+      [strongConnector childDirectedTreatment]) {
+    [FBAdSettings setIsChildDirected:[[strongConnector childDirectedTreatment] boolValue]];
+  }
+
+  [_bannerAd getBannerWithSize:adSize];
+}
+
+- (void)getInterstitial {
+  id<GADMAdNetworkConnector> strongConnector = _connector;
+  if ([strongConnector respondsToSelector:@selector(childDirectedTreatment)] &&
+      [strongConnector childDirectedTreatment]) {
+    [FBAdSettings setIsChildDirected:[[strongConnector childDirectedTreatment] boolValue]];
+  }
+
+  [_interstitialAd getInterstitial];
+}
+
+- (void)getNativeAdWithAdTypes:(NSArray *)adTypes options:(NSArray *)options {
+  id<GADMAdNetworkConnector> strongConnector = _connector;
+  if ([strongConnector respondsToSelector:@selector(childDirectedTreatment)] &&
+      [strongConnector childDirectedTreatment]) {
+    [FBAdSettings setIsChildDirected:[strongConnector childDirectedTreatment].boolValue];
+  }
+
+  [_nativeAd getNativeAdWithAdTypes:adTypes options:options];
+}
+
+- (void)stopBeingDelegate {
+  [_bannerAd stopBeingDelegate];
+  [_interstitialAd stopBeingDelegate];
+  [_nativeAd stopBeingDelegate];
+}
+
+- (BOOL)isBannerAnimationOK:(GADMBannerAnimationType)animType {
+  return YES;
+}
+
+- (void)presentInterstitialFromRootViewController:(UIViewController *)rootViewController {
+  [_interstitialAd presentInterstitialFromRootViewController:rootViewController];
+}
+
+- (BOOL)handlesUserClicks {
+  return YES;
+}
+
+- (BOOL)handlesUserImpressions {
+  return YES;
+}
+
+@end
