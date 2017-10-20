db23da3df4eeb0aa506c00e159ad5fa8eb0e8806
Andrey Seredkin <aserrr@users.noreply.github.com>
2017-10-21 01:37:59 +0300

    Added myTarget adapter files (#26)
    * Added adapter files
    
    * Ignore SDK folders
    
    * Updated myTarget adapter (allow empty headline/body in Native Ad). Logger improved.
    
    * Revert "Ignore SDK folders"
    
    This reverts commit 93ab75b838d12cb159478666f797bca2f352345a.
    
    * Updated myTarget adapter (added validation for assets, minor bugs fixed).
    
    * Updated myTarget adapter. Added an extra assets for Native ad: advertising label, age restrictions, category/subcategory, votes.

---
+++ adapters/MyTarget/MyTargetAdapter/GADMAdapterMyTargetUtils.h
@@ -0,0 +1,32 @@
+//
+//  GADMAdapterMyTargetUtils.h
+//  MyTargetAdapter
+//
+//  Created by Andrey Seredkin on 28.09.17.
+//  Copyright © 2017 Mail.Ru Group. All rights reserved.
+//
+
+@import GoogleMobileAds;
+@import MyTargetSDK;
+
+#define MTRGLogInfo() if ([GADMAdapterMyTargetUtils isLogEnabled]) { NSLog(@"[%@ info] %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd)); }
+#define MTRGLogDebug(format, ...) if ([GADMAdapterMyTargetUtils isLogEnabled]) { NSLog(@"[%@ debug] %@", NSStringFromClass([self class]), [NSString stringWithFormat:(format), ##__VA_ARGS__]); }
+#define MTRGLogError(message) if ([GADMAdapterMyTargetUtils isLogEnabled]) { NSLog(@"[%@ error] %@", NSStringFromClass([self class]), (message)); }
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GADMAdapterMyTargetUtils : NSObject
+
++ (BOOL)isLogEnabled;
++ (void)setLogEnabled:(BOOL)isLogEnabled;
++ (NSError *)errorWithDescription:(NSString *)description;
++ (NSString *)noAdWithReason:(NSString *)reason;
++ (NSUInteger)slotIdFromCredentials:(NSDictionary *)credentials;
++ (void)fillCustomParams:(MTRGCustomParams *)customParams withConnector:(nullable id<GADMediationAdRequest>)connector;
++ (MTRGGender)genderFromAdmobGender:(GADGender)admobGender;
++ (nullable NSNumber *)ageFromBirthday:(NSDate *)birthday;
++ (BOOL)isSize:(GADAdSize)size1 equalToSize:(GADAdSize)size2;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ adapters/MyTarget/Public/Headers/GADMAdapterMyTargetExtraAssets.h
@@ -0,0 +1,19 @@
+//
+//  GADMAdapterMyTargetExtraAssets.h
+//  MyTargetAdapter
+//
+//  Created by Andrey Seredkin on 20.10.17.
+//  Copyright © 2017 Mail.Ru Group. All rights reserved.
+//
+
+@import Foundation;
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString *const kGADMAdapterMyTargetExtraAssetAdvertisingLabel;
+extern NSString *const kGADMAdapterMyTargetExtraAssetAgeRestrictions;
+extern NSString *const kGADMAdapterMyTargetExtraAssetCategory;
+extern NSString *const kGADMAdapterMyTargetExtraAssetSubcategory;
+extern NSString *const kGADMAdapterMyTargetExtraAssetVotes;
+
+NS_ASSUME_NONNULL_END
