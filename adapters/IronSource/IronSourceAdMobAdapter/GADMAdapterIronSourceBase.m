f4a85d8c811fff96abf5c03153bfe9327b2380c0
Gili Ariel <gili.ariel@ironsrc.com>
2017-12-26 11:17:14 +0200

    Initiate IronSource’s SDK only once.

--- adapters/IronSource/IronSourceAdMobAdapter/GADMAdapterIronSourceBase.m
+++ adapters/IronSource/IronSourceAdMobAdapter/GADMAdapterIronSourceBase.m
@@ -15,7 +15,9 @@
 
 #import "GADMAdapterIronSourceBase.h"
 
-@implementation GADMAdapterIronSourceBase
+@implementation GADMAdapterIronSourceBase {
+    BOOL _initSucceeded;
+}
 
 
 // IronSource internal reporting const
@@ -44,10 +46,12 @@ NSString *const kGADMAdapterIronSourceIsTestEnabled = @"isTestEnabled";
     // 1 - We are not sending user ID from adapters anymore,
     //     the IronSource SDK will take care of this identifier
     // 2 - We assume the init is always successful (we will fail in load if needed)
-    
-    [self onLog:@"initIronSourceSDKWithAppKey"];
-    [IronSource setMediationType:kGADMMediationName];
-    [IronSource initWithAppKey:appKey adUnits:@[adUnit]];
+    if (!_initSucceeded) {
+        [self onLog:@"initIronSourceSDKWithAppKey"];
+        [IronSource setMediationType:kGADMMediationName];
+        [IronSource initWithAppKey:appKey adUnits:@[adUnit]];
+        _initSucceeded = YES;
+    }
 }
 
 - (void)onLog: (NSString *)log {
