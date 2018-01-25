952230c4568824ee821150f151ee6fefd26f8a50
Thomas So <thomas.m.so33@gmail.com>
2018-01-25 12:21:05 -0800

    Fix comment

--- adapters/AppLovin/Public/Headers/GADMAdapterAppLovinExtras.h
+++ adapters/AppLovin/Public/Headers/GADMAdapterAppLovinExtras.h
@@ -11,7 +11,9 @@
 
 @interface GADMAdapterAppLovinExtras : NSObject<GADAdNetworkExtras>
 
-// Optional: Disable audio for video ads, must be set on each ad request.
+/**
+ * Disables audio for video ads. Must be set on each ad request.
+ */
 @property (nonatomic, assign) BOOL muteAudio;
 
 /**
