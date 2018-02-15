41f39c8a08176b233733cd3b02741c8909b01aeb
Santosh Bagadi <sbagadi@google.com>
2018-02-15 09:20:53 -0800

    Updating Chartboost adapter to version 7.1.0.0.

--- adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostConstants.h
+++ adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostConstants.h
@@ -15,7 +15,7 @@
 @import Foundation;
 
 /// Chartboost mediation network adapter version.
-static NSString *const GADMAdapterChartboostVersion = @"7.0.4.0";
+static NSString *const GADMAdapterChartboostVersion = @"7.1.0.0";
 
 /// Chartboost App ID.
 static NSString *const GADMAdapterChartboostAppID = @"appId";
