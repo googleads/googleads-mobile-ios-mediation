855d96fa7be92c52d84bb984f1b9eb80dfdc718a
rampara <rampara@google.com>
2018-05-16 14:46:38 -0700

    Revert "AppLovin/Add Native Ads Adapter"

--- adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinUtils.h
+++ adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinUtils.h
@@ -34,8 +34,6 @@ NS_ASSUME_NONNULL_BEGIN
 + (ALIncentivizedInterstitialAd *)incentivizedInterstitialAdWithZoneIdentifier:
                                       (NSString *)zoneIdentifier sdk:(ALSdk *)sdk;
 
-+ (void)log:(NSString *)format, ...;
-
 @end
 
 NS_ASSUME_NONNULL_END
