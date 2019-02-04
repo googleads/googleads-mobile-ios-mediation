d135ddb3f73efdb751a1c0313388e62afeb62be8
maninomula <nomulam@google.com>
2019-02-04 11:09:43 -0800

    Updating InMobi adapter to version 7.2.4.0.

--- adapters/InMobi/InMobiAdapter/GADMAdapterInMobi.m
+++ adapters/InMobi/InMobiAdapter/GADMAdapterInMobi.m
@@ -43,7 +43,7 @@ __attribute__((constructor)) static void initialize_imageCache() {
 @synthesize connector = connector_;
 
 + (NSString *)adapterVersion {
-  return @"7.2.1.0";
+  return @"7.2.4.0";
 }
 
 + (BOOL) isAppInitialised {
