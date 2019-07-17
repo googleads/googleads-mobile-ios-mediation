37a5ac2886f04ad7088decc49e41838462a3ddef
Bar David <bar.david@ironsrc.com>
2019-07-17 10:41:15 +0300

    remove duplications from GADMAdapterIronSourceUtils

--- adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceUtils.m
+++ adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceUtils.m
@@ -57,23 +57,4 @@
     return version;
 }
 
-+ (void)onLog:(NSString *)log {
-  NSLog(@"IronSourceAdapter: %@", log);
-}
-
-+ (NSString *)getAdMobSDKVersion {
-  NSString *version = @"";
-  NSString *sdkVersion = [GADRequest sdkVersion];
-  @try {
-    NSUInteger versionIndex = [sdkVersion rangeOfString:@"-v"].location + 1;
-    version = [sdkVersion substringFromIndex:versionIndex];
-    version = [version stringByReplacingOccurrencesOfString:@"." withString:@""];
-
-  } @catch (NSException *exception) {
-    NSLog(@"Unable to parse AdMob SDK version");
-    version = @"";
-  }
-  return version;
-}
-
 @end
