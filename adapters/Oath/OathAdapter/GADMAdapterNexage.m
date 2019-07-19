614ff08aecb65d7caa363b93f7f65a9752967283
Manikanta Nomula <nomulam@google.com>
2019-07-18 17:10:50 -0700

    Changing code style of the Oath adapter to follow Google's style guide.
    PiperOrigin-RevId: 258875381

--- adapters/Oath/OathAdapter/GADMAdapterNexage.m
+++ adapters/Oath/OathAdapter/GADMAdapterNexage.m
@@ -8,44 +8,40 @@
 #import <GoogleMobileAds/GADRequest.h>
 
 static NSString *const kGADNexagePosition = @"position";
-static NSString *const kGADNexageDCN      = @"dcn";
-static NSString *const kGADVerizonSiteId  = @"VerizonSiteID";
+static NSString *const kGADNexageDCN = @"dcn";
+static NSString *const kGADVerizonSiteId = @"VerizonSiteID";
 
 @implementation GADMAdapterNexage
 
-- (id)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)gadConnector
-{
-    if (self = [super initWithGADMAdNetworkConnector:gadConnector]) {
-        [self initializeVASAds];
-    }
+- (id)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)gadConnector {
+  if (self = [super initWithGADMAdNetworkConnector:gadConnector]) {
+    [self initializeVASAds];
+  }
 
-    return self;
+  return self;
 }
 
-- (void)initializeVASAds
-{
-    //Position
-    NSDictionary *credentials = [self.connector credentials];
-    if(credentials[kGADNexagePosition] != nil)
-    {
-        self.placementID = credentials[kGADNexagePosition];
-    }
+- (void)initializeVASAds {
+  // Position
+  NSDictionary *credentials = [self.connector credentials];
+  if (credentials[kGADNexagePosition] != nil) {
+    self.placementID = credentials[kGADNexagePosition];
+  }
 
-    //Site ID
-    NSString *siteId = credentials[kGADNexageDCN];
-    if (siteId.length == 0) {
-        siteId = [[NSBundle mainBundle] objectForInfoDictionaryKey:kGADVerizonSiteId];
-    }
+  // Site ID
+  NSString *siteId = credentials[kGADNexageDCN];
+  if (siteId.length == 0) {
+    siteId = [[NSBundle mainBundle] objectForInfoDictionaryKey:kGADVerizonSiteId];
+  }
 
-    if([[UIDevice currentDevice] systemVersion].floatValue >= 8.0) {
-        VASAds.logLevel = VASLogLevelError;
+  if ([[UIDevice currentDevice] systemVersion].floatValue >= 8.0) {
+    VASAds.logLevel = VASLogLevelError;
 
-        if([VASAds sharedInstance].initialized == NO) {
-            [VASStandardEdition initializeWithSiteId:siteId];
-        }
-        self.vasAds = [VASAds sharedInstance];
+    if ([VASAds sharedInstance].initialized == NO) {
+      [VASStandardEdition initializeWithSiteId:siteId];
     }
+    self.vasAds = [VASAds sharedInstance];
+  }
 }
-
 
 @end
--- adapters/Oath/OathAdapter/GADMediationAdapterVerizon.m
+++ adapters/Oath/OathAdapter/GADMediationAdapterVerizon.m
@@ -8,43 +8,39 @@
 #import "GADMediationAdapterVerizon.h"
 
 static NSString *const kGADVerizonPosition = @"placement_id";
-static NSString *const kGADVerizonDCN      = @"site_id";
-static NSString *const kGADVerizonSiteId   = @"VerizonSiteID";
+static NSString *const kGADVerizonDCN = @"site_id";
+static NSString *const kGADVerizonSiteId = @"VerizonSiteID";
 
 @implementation GADMediationAdapterVerizon
 
-- (id)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)gadConnector
-{
-    if (self = [super initWithGADMAdNetworkConnector:gadConnector]) {
-        [self initializeVASAds];
-    }
-    return self;
+- (id)initWithGADMAdNetworkConnector:(id<GADMAdNetworkConnector>)gadConnector {
+  if (self = [super initWithGADMAdNetworkConnector:gadConnector]) {
+    [self initializeVASAds];
+  }
+  return self;
 }
 
-- (void)initializeVASAds
-{
-    //Position
-    NSDictionary *credentials = [self.connector credentials];
-    if(credentials[kGADVerizonPosition] != nil)
-    {
-        self.placementID = credentials[kGADVerizonPosition];
-    }
+- (void)initializeVASAds {
+  // Position
+  NSDictionary *credentials = [self.connector credentials];
+  if (credentials[kGADVerizonPosition] != nil) {
+    self.placementID = credentials[kGADVerizonPosition];
+  }
 
-    //Site ID
-    NSString *siteId = credentials[kGADVerizonDCN];
-    if (siteId.length == 0) {
-        siteId = [[NSBundle mainBundle] objectForInfoDictionaryKey:kGADVerizonSiteId];
-    }
+  // Site ID
+  NSString *siteId = credentials[kGADVerizonDCN];
+  if (siteId.length == 0) {
+    siteId = [[NSBundle mainBundle] objectForInfoDictionaryKey:kGADVerizonSiteId];
+  }
 
-    if([[UIDevice currentDevice] systemVersion].floatValue >= 8.0) {
-        VASAds.logLevel = VASLogLevelError;
+  if ([[UIDevice currentDevice] systemVersion].floatValue >= 8.0) {
+    VASAds.logLevel = VASLogLevelError;
 
-        if([VASAds sharedInstance].initialized == NO) {
-            [VASStandardEdition initializeWithSiteId:siteId];
-        }
-        self.vasAds = [VASAds sharedInstance];
+    if ([VASAds sharedInstance].initialized == NO) {
+      [VASStandardEdition initializeWithSiteId:siteId];
     }
+    self.vasAds = [VASAds sharedInstance];
+  }
 }
-
 
 @end
