0302ce8d73c1468db0e602fd8a95859e4fb575ab
Eric Leichtenschlag <eleichtenschl@google.com>
2019-07-29 16:51:51 -0700

    Internal change.
    PiperOrigin-RevId: 260609202

--- adapters/Facebook/BuildScript/Script_Validate.sh
+++ adapters/Facebook/BuildScript/Script_Validate.sh
@@ -1,15 +1,1 @@
 #!/bin/bash
-
-# copybara:strip_begin
-set -o errexit
-set -o nounset
-set -o xtrace
-
-SCRIPT_PATH="$( cd "$( dirname "$0" )" && pwd )"
-GOOGLE3_ROOT="${SCRIPT_PATH}/../../../../../../../google3"
-NONAGON_ROOT="${GOOGLE3_ROOT}/googlemac/iPhone/GoogleAds/GoogleMobileAdsNonagon/Nonagon"
-PROHIBIT_PATTERNS_SCRIPT="${NONAGON_ROOT}/scripts/prohibit_patterns.sh"
-
-bash "${PROHIBIT_PATTERNS_SCRIPT}" "${SCRIPT_PATH}/../Public"
-bash "${PROHIBIT_PATTERNS_SCRIPT}" "${SCRIPT_PATH}/../FacebookAdapter"
-# copybara:strip_end
