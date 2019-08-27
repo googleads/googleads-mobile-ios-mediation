57036808089e09889351f1f60c67ec3d0a9e602c
Manikanta Nomula <nomulam@google.com>
2019-08-27 13:00:37 -0700

    Updating Chartboost adpapter to version 8.0.1.0.
    PiperOrigin-RevId: 265747384

--- adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostConstants.h
+++ adapters/Chartboost/ChartboostAdapter/GADMAdapterChartboostConstants.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 
 /// Chartboost mediation network adapter version.
-static NSString *const kGADMAdapterChartboostVersion = @"7.5.0.1";
+static NSString *const kGADMAdapterChartboostVersion = @"8.0.1.0";
 
 /// Chartboost App ID.
 static NSString *const kGADMAdapterChartboostAppID = @"appId";
