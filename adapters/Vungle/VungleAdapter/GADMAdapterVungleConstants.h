06d414e87708afb29b87d33919abbf5973c9bed2
YueVungle <yueting.cui@vungle.com>
2019-08-28 13:07:20 +0800

    Update adapter version number to 6.4.3.0

--- adapters/Vungle/VungleAdapter/GADMAdapterVungleConstants.h
+++ adapters/Vungle/VungleAdapter/GADMAdapterVungleConstants.h
@@ -2,7 +2,7 @@
 //  Copyright © 2019 Google. All rights reserved.
 //
 
-static NSString *const kGADMAdapterVungleVersion = @"6.4.2.1";
+static NSString *const kGADMAdapterVungleVersion = @"6.4.3.0";
 static NSString *const kGADMAdapterVungleApplicationID = @"application_id";
 static NSString *const kGADMAdapterVunglePlacementID = @"placementID";
 static NSString *const kGADMAdapterVungleErrorDomain =
