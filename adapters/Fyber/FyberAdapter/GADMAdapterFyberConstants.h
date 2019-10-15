d7c098f57614806deca8644f60610185d239764b
avi gelkop <avi.gelkop@fyber.com>
2019-10-15 14:19:02 +0300

    Implemented Fyber adapters

--- adapters/Fyber/FyberAdapter/GADMAdapterFyberConstants.h
+++ adapters/Fyber/FyberAdapter/GADMAdapterFyberConstants.h
@@ -1,21 +1,12 @@
-// Copyright 2019 Google LLC.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+//  GADMAdapterFyberConstants.h
+//  Adapter
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//  Created by Fyber on 10/15/19.
+//  Copyright © 2019 Google. All rights reserved.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-/// Adapter version string.
-static NSString *const kGADMAdapterFyberVersion = @"7.3.0.0";
-
-/// Adapter error domain.
-static NSString *const kGADMAdapterFyberErrorDomain = @"com.google.mediation.fyber";
-
-
+static NSString * const _Nonnull kFYBAdapterVersion = @"1.0.0";
+static NSString * const _Nonnull kGADMAdapterFyberErrorDomain = @"com.google.mediation.fyber";
+static NSString * const _Nonnull kFYBApplicationID = @"applicationId";
+static NSString * const _Nonnull kFYBSpotID = @"spotId";
