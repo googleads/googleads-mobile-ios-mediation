e39035b46b83fc83efc44cd38bb3c9aa75bc6d9b
Manikanta Nomula <nomulam@google.com>
2020-01-24 12:37:29 -0800

    Updating IronSource adapter to version 6.11.0.0.
    PiperOrigin-RevId: 291422358

--- adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceConstants.h
+++ adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceConstants.h
@@ -14,7 +14,7 @@
 
 // IronSource internal reporting const.
 static NSString *const kGADMAdapterIronSourceMediationName = @"AdMob";
-static NSString *const kGADMAdapterIronSourceAdapterVersion = @"6.8.7.0";
+static NSString *const kGADMAdapterIronSourceAdapterVersion = @"6.11.0.0";
 static NSString *const kGADMIronSourceDefaultInstanceId = @"0";
 static NSString *const kGADMAdapterIronSourceInternalVersion = @"310";
 
