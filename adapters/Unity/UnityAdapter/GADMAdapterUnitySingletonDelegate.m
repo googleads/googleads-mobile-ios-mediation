7d3a8819efbb5881b1771a41378e2404597bf1ca
Jindou Jiao <alexandrajjd@gmail.com>
2020-01-31 14:21:48 -0800

    add singletonDelegate
    add singletonDelegate

---
+++ adapters/Unity/UnityAdapter/GADMAdapterUnitySingletonDelegate.m
@@ -0,0 +1,9 @@
+//
+//  GADMAdapterUnitySingletonDelegate.m
+//  AdMob-TestApp-Local
+//
+//  Created by Jindou Jiao on 01/30/2020.
+//  Copyright © 2020 Unity Ads. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
