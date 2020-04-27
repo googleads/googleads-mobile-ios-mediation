f7d7d72144beda3c7a80e36f1da0a7f81be3fde6
Joshua James Pascual Lagonera <joshuaja@google.com>
2020-04-27 09:38:09 -0700

    Verified AppLovin iOS adapter compatibility with AppLovin SDK 6.12.1
    PiperOrigin-RevId: 308637644

--- adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinConstant.m
+++ adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinConstant.m
@@ -14,7 +14,7 @@ NSString *const GADMAdapterAppLovinSDKErrorDomain = @"com.google.mediation.applo
 
 NSString *const GADMAdapterAppLovinInfoPListSDKKey = @"AppLovinSdkKey";
 
-NSString *const GADMAdapterAppLovinAdapterVersion = @"6.12.0.0";
+NSString *const GADMAdapterAppLovinAdapterVersion = @"6.12.1.0";
 
 NSString *const GADMAdapterAppLovinSDKKey = @"sdkKey";
 
