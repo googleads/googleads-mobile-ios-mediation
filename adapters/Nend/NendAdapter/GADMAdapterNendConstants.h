3610b366f00bdd273a8f81b147dd535a99e262a6
Joshua James Pascual Lagonera <joshuaja@google.com>
2021-01-11 13:00:21 -0800

    Updating nend iOS adapter for release version 7.0.2.0
    PiperOrigin-RevId: 351217158

--- adapters/Nend/NendAdapter/GADMAdapterNendConstants.h
+++ adapters/Nend/NendAdapter/GADMAdapterNendConstants.h
@@ -13,7 +13,7 @@
 // limitations under the License.
 
 // Adapter version string
-static NSString *const _Nonnull kGADMAdapterNendVersion = @"7.0.1.0";
+static NSString *const _Nonnull kGADMAdapterNendVersion = @"7.0.2.0";
 
 // Adapter mediation agent
 static NSString *const _Nonnull kGADMAdapterNendMediationName = @"AdMob";
