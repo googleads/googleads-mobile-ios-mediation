6c8c7adef38cf184946896a429234f7bbed57747
im-ttmskk <tatsu-kasamatsu@i-mobile.co.jp>
2021-03-05 01:04:16 +0900

    Use instance variables: GADRTBMaioRewardedAd

--- adapters/Maio/MaioAdapter/RTBAdapter/GADRTBMaioRewardedAd.m
+++ adapters/Maio/MaioAdapter/RTBAdapter/GADRTBMaioRewardedAd.m
@@ -13,30 +13,30 @@
 
 @interface GADRTBMaioRewardedAd () <MaioRewardedLoadCallback, MaioRewardedShowCallback>
 
-@property (nonatomic, copy) GADMediationRewardedLoadCompletionHandler completionHandler;
-@property (nonatomic, weak) id<GADMediationRewardedAdEventDelegate> adEventDelegate;
-@property (nonatomic, strong) MaioRewarded *rewarded;
-
 @end
 
-@implementation GADRTBMaioRewardedAd
+@implementation GADRTBMaioRewardedAd {
+  GADMediationRewardedLoadCompletionHandler _completionHandler;
+  __weak id<GADMediationRewardedAdEventDelegate> _adEventDelegate;
+  MaioRewarded *_rewarded;
+}
 
 - (void)loadRewardedAdForAdConfiguration:(nonnull GADMediationRewardedAdConfiguration *)adConfiguration
                        completionHandler:(nonnull GADMediationRewardedLoadCompletionHandler)completionHandler {
-  self.completionHandler = completionHandler;
+  _completionHandler = completionHandler;
 
   MaioRequest *request = [[MaioRequest alloc] initWithZoneId:kGADRTBMaioAdapterZoneId testMode:adConfiguration.isTestRequest bidData:adConfiguration.bidResponse];
-  self.rewarded = [MaioRewarded loadAdWithRequest:request callback:self];
+  _rewarded = [MaioRewarded loadAdWithRequest:request callback:self];
 }
 
 - (void)presentFromViewController:(nonnull UIViewController *)viewController {
-  [self.rewarded showWithViewContext:viewController callback:self];
+  [_rewarded showWithViewContext:viewController callback:self];
 }
 
 #pragma mark - MaioRewardedLoadCallback, MaioRewardedShowCallback
 
 - (void)didLoad:(MaioRewarded *)ad {
-  self.adEventDelegate = self.completionHandler(self, nil);
+  _adEventDelegate = _completionHandler(self, nil);
 }
 
 - (void)didFail:(MaioRewarded *)ad errorCode:(NSInteger)errorCode {
@@ -45,24 +45,24 @@
   NSError *error = [NSError errorWithDomain:kGADMMaioSDKErrorDomain code:errorCode userInfo:userInfo];
 
   if (10000 <= errorCode && errorCode < 20000) {
-    self.completionHandler(nil, error);
+    _completionHandler(nil, error);
     return;
   }
   if (20000 <= errorCode && errorCode < 30000) {
-    [self.adEventDelegate didFailToPresentWithError:error];
+    [_adEventDelegate didFailToPresentWithError:error];
     return;
   }
 }
 
 - (void)didOpen:(MaioRewarded *)ad {
-  id<GADMediationRewardedAdEventDelegate> delegate = self.adEventDelegate;
+  id<GADMediationRewardedAdEventDelegate> delegate = _adEventDelegate;
   [delegate willPresentFullScreenView];
   [delegate reportImpression];
   [delegate didStartVideo];
 }
 
 - (void)didClose:(MaioRewarded *)ad {
-  id<GADMediationRewardedAdEventDelegate> delegate = self.adEventDelegate;
+  id<GADMediationRewardedAdEventDelegate> delegate = _adEventDelegate;
   [delegate didEndVideo];
   [delegate willDismissFullScreenView];
   [delegate didDismissFullScreenView];
@@ -71,7 +71,7 @@
 - (void)didReward:(MaioRewarded *)ad reward:(RewardData *)reward {
   GADAdReward *gReward = [[GADAdReward alloc] initWithRewardType:reward.value rewardAmount:[NSDecimalNumber one]];
 
-  id<GADMediationRewardedAdEventDelegate> delegate = self.adEventDelegate;
+  id<GADMediationRewardedAdEventDelegate> delegate = _adEventDelegate;
   [delegate didRewardUserWithReward:gReward];
 }
 
