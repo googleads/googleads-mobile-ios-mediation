1cbc6c83e75c72b6f893148b1efd2b2bed5ec616
Mobile Ads Developer Relations <mad-devrel@google.com>
2021-07-14 09:50:27 -0700

    Updating Unity iOS Adapter to v3.7.4.0
    PiperOrigin-RevId: 384720787

--- adapters/Unity/UnityAdapter/GADMAdapterUnityConstants.h
+++ adapters/Unity/UnityAdapter/GADMAdapterUnityConstants.h
@@ -29,7 +29,7 @@ static NSString *const kGADMAdapterUnityGameID = @"gameId";
 static NSString *const kGADMAdapterUnityPlacementID = @"zoneId";
 
 /// Ad mediation network adapter version.
-static NSString *const kGADMAdapterUnityVersion = @"3.7.2.0";
+static NSString *const kGADMAdapterUnityVersion = @"3.7.4.0";
 
 /// Ad mediation network name.
 static NSString *const kGADMAdapterUnityMediationNetworkName = @"AdMob";
