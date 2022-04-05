8aab2ba19c5db694077a3fb401c86b2527280a62
Mobile Ads Developer Relations <mad-devrel@google.com>
2022-04-05 12:16:59 -0700

    Updating ironSource iOS Adapter to v7.2.1.2.0
    PiperOrigin-RevId: 439644731

--- adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceConstants.h
+++ adapters/IronSource/IronSourceAdapter/GADMAdapterIronSourceConstants.h
@@ -14,7 +14,7 @@
 
 // IronSource internal reporting const.
 static NSString *const GADMAdapterIronSourceMediationName = @"AdMob";
-static NSString *const GADMAdapterIronSourceAdapterVersion = @"7.2.1.1.0";
+static NSString *const GADMAdapterIronSourceAdapterVersion = @"7.2.1.2.0";
 static NSString *const GADMIronSourceDefaultInstanceId = @"0";
 static NSString *const GADMAdapterIronSourceInternalVersion = @"310";
 
