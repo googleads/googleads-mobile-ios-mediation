e2baa33204c4775f05ede1d81c20f8b69fdf6bfa
yy <wangyuanyuan.yy007@bytedance.com>
2022-09-20 21:41:31 +0800

    changed version

--- adapters/Pangle/PangleAdapter/GADMediationAdapterPangleConstants.h
+++ adapters/Pangle/PangleAdapter/GADMediationAdapterPangleConstants.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 
 /// Pangle adapter version.
-static NSString *const GADMAdapterPangleVersion = @"4.6.2.2.0";
+static NSString *const GADMAdapterPangleVersion = @"4.6.2.2.1";
 
 /// Pangle adapter Pangle App ID parameter key.
 static NSString *const GADMAdapterPangleAppID = @"appid";
