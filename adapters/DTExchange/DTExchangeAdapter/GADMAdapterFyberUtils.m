514e27a164ac5468a8de935ae2a0141cd44e7967
Nikita Fedorenko <nikita.fedorenko@digitalturbine.com>
2023-04-04 19:35:45 +0300

    DTExchange 8.2.0 version  support

--- adapters/DTExchange/DTExchangeAdapter/GADMAdapterFyberUtils.m
+++ adapters/DTExchange/DTExchangeAdapter/GADMAdapterFyberUtils.m
@@ -72,7 +72,7 @@ IAAdRequest *_Nonnull GADMAdapterFyberBuildRequestWithSpotIDAndAdConfiguration(
     builder.spotID = spotID;
     builder.timeout = 10;
     builder.userData = extras.userData;
-    builder.muteAudio = extras.muteAudio;
+    IASDKCore.sharedInstance.muteAudio = extras.muteAudio;
     if (keywords) {
       builder.keywords = keywords;
     }
