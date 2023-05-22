7326dc042ab9730e8aa908816164412476ac9abf
yy <wangyuanyuan.yy007@bytedance.com>
2023-05-22 16:20:48 +0800

    fix

--- adapters/Pangle/PangleAdapter/Renderer/GADPangleAppOpenRenderer.m
+++ adapters/Pangle/PangleAdapter/Renderer/GADPangleAppOpenRenderer.m
@@ -1,9 +1,16 @@
+// Copyright 2023 Google LLC
 //
-//  GADPangleAppOpenRenderer.m
-//  Adapter
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-//  Created by bytedance on 2023/5/5.
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #import "GADPangleAppOpenRenderer.h"
 #import <PAGAdSDK/PAGAdSDK.h>
