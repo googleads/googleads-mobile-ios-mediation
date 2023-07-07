b3088290b87bacff33f115c5f6eef2462304a2b5
rajat-kasliwal-inmobi <rajat.kasliwal.inmobi@gmail.com>
2023-07-07 15:38:36 +0530

    Changes in adapter for US Privacy String

--- adapters/InMobi/InMobiAdapter/GADMAdapterInMobiInterstitialAd.m
+++ adapters/InMobi/InMobiAdapter/GADMAdapterInMobiInterstitialAd.m
@@ -102,6 +102,7 @@
   }
 
   GADMAdapterInMobiSetTargetingFromAdConfiguration(_interstitialAdConfig);
+  GADMAdapterInMobiSetPrivacyCompliance();
   NSDictionary<NSString *, id> *requestParameters =
       GADMAdapterInMobiCreateRequestParametersFromAdConfiguration(_interstitialAdConfig);
   [_interstitialAd setExtras:requestParameters];
