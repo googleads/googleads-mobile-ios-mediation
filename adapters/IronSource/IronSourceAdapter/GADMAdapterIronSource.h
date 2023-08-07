6eeba6be4cd5b9a174b56ff1fc216b6fa3c00f18
Alon Dotan <alond@unity3d.com>
2023-08-07 14:26:59 +0300

    Update GADMAdapterIronSource.h
    Fix comment

--- adapters/IronSource/IronSourceAdapter/GADMAdapterIronSource.h
+++ adapters/IronSource/IronSourceAdapter/GADMAdapterIronSource.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 #import <GoogleMobileAds/GoogleMobileAds.h>
 
-/// Adapter for communicating with the IronSource Network to fetch nterstitial video ads.
+/// Adapter for communicating with the IronSource Network to fetch interstitial and banner ads.
 @interface GADMAdapterIronSource : NSObject
 
 @end
