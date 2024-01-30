db135ebaf846490a7a5bb80840aad5fe1a87e1df
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2024-01-29 17:14:36 -0800

    Version bump.
    PiperOrigin-RevId: 602545209

--- adapters/Line/LineAdapter/GADMediationAdapterLineConstants.m
+++ adapters/Line/LineAdapter/GADMediationAdapterLineConstants.m
@@ -14,7 +14,7 @@
 
 #import "GADMediationAdapterLineConstants.h"
 
-NSString *const GADMediationAdapterLineVersion = @"2.7.20231115.0";
+NSString *const GADMediationAdapterLineVersion = @"2.7.20240126.0";
 
 NSString *const GADMediationAdapterLineCredentialKeyApplicationID = @"application_id";
 
