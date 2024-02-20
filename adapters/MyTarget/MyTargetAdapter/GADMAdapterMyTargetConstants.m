51577dfec99bbe28fa6b51ea7bfa6c74612430e3
Mobile Ads Developer Relations <mad-devrel-github-bot@google.com>
2024-02-19 16:26:13 -0800

    Updating MyTarget iOS adapter for release version 5.21.0
    PiperOrigin-RevId: 608418602

--- adapters/MyTarget/MyTargetAdapter/GADMAdapterMyTargetConstants.m
+++ adapters/MyTarget/MyTargetAdapter/GADMAdapterMyTargetConstants.m
@@ -14,7 +14,7 @@
 
 #import "GADMAdapterMyTargetConstants.h"
 
-NSString *const _Nonnull GADMAdapterMyTargetVersion = @"5.20.2.0";
+NSString *const _Nonnull GADMAdapterMyTargetVersion = @"5.21.0.0";
 
 NSString *const _Nonnull GADMAdapterMyTargetSlotIdKey = @"slotId";
 
