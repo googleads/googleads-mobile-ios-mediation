b30dee111fd7c634dc5a71aab19f62100b41dd03
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-04-25 13:26:22 -0700

    Updating Pangle iOS adapter for release version 5.9.0.7.
    PiperOrigin-RevId: 628172690

--- adapters/Pangle/PangleAdapter/GADMediationAdapterPangleConstants.h
+++ adapters/Pangle/PangleAdapter/GADMediationAdapterPangleConstants.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 
 /// Pangle adapter version.
-static NSString *const GADMAdapterPangleVersion = @"5.9.0.6.0";
+static NSString *const GADMAdapterPangleVersion = @"5.9.0.7.0";
 
 /// Pangle adapter Pangle App ID parameter key.
 static NSString *const GADMAdapterPangleAppID = @"appid";
