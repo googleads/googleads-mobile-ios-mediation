4c5982c6e0d65976f9bd81b94b0b89316abec621
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-08-19 13:56:53 -0700

    Configure FakeMolocoRewarded
    PiperOrigin-RevId: 664966125

--- AdapterUnitTests/MolocoAdapterTests/Fakes/FakeMolocoRewarded.swift
+++ AdapterUnitTests/MolocoAdapterTests/Fakes/FakeMolocoRewarded.swift
@@ -19,13 +19,23 @@ import MolocoSDK
 /// A fake implementation of MolocoRewarded.
 final class FakeMolocoRewarded {
 
+  /// The load error that occured.
+  let loadError: Error?
+
+  /// Var to capture the bid response that was used to load the ad on Moloco SDK. Used for
+  /// assertion. It is initlialized to a value that is never asserted for.
+  var bidResponseUsedToLoadMolocoAd: String = "nil"
+
   // MolocoSDK.MolocoRewardedInterstitial properties.
   var rewardedDelegate: (any MolocoSDK.MolocoRewardedDelegate)?
   var fullscreenViewController: UIViewController?
   var isReady: Bool
 
-  init() {
-    // TODO(b/359236741): Implement init logic.
+  /// If loadError is nil, this fake mimics load success. If loadError is not nil, this fake mimics
+  /// load failure.
+  init(rewardedDelegate: any MolocoSDK.MolocoRewardedDelegate, loadError: Error?) {
+    self.rewardedDelegate = rewardedDelegate
+    self.loadError = loadError
     self.isReady = false
   }
 
@@ -44,7 +54,12 @@ extension FakeMolocoRewarded: MolocoSDK.MolocoRewardedInterstitial {
   }
 
   func load(bidResponse: String) {
-    // No-op.
+    bidResponseUsedToLoadMolocoAd = bidResponse
+    guard let loadError else {
+      rewardedDelegate?.didLoad(ad: self)
+      return
+    }
+    rewardedDelegate?.failToLoad(ad: self, with: loadError)
   }
 
   func destroy() {
