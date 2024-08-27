834a7148aa0670cec6a43e609133399abbe26c30
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-08-27 10:13:58 -0700

    Updating AppLovin iOS adapter for release version 12.6.1.
    PiperOrigin-RevId: 668037350

--- adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinConstant.m
+++ adapters/AppLovin/AppLovinAdapter/GADMAdapterAppLovinConstant.m
@@ -18,7 +18,7 @@ NSString *const GADMAdapterAppLovinErrorDomain = @"com.google.mediation.applovin
 
 NSString *const GADMAdapterAppLovinSDKErrorDomain = @"com.google.mediation.applovinSDK";
 
-NSString *const GADMAdapterAppLovinAdapterVersion = @"12.6.0.0";
+NSString *const GADMAdapterAppLovinAdapterVersion = @"12.6.1.0";
 
 NSString *const GADMAdapterAppLovinSDKKey = @"sdkKey";
 
