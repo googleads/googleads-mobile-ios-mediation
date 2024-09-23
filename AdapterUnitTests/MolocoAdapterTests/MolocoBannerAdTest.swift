08a87acfe8a1dfa0a133ddc70b004b493ee0cd3c
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2024-09-23 10:52:18 -0700

    Add Moloco Adapter Banner Test Skeleton Files
    PiperOrigin-RevId: 677858510

---
+++ AdapterUnitTests/MolocoAdapterTests/MolocoBannerAdTest.swift
@@ -0,0 +1,11 @@
+import AdapterUnitTestKit
+import MolocoSDK
+import XCTest
+
+@testable import MolocoAdapter
+
+final class MolocoBannerAdTest: XCTestCase {
+
+  // TODO: b/368608855 - Add tests.
+
+}
