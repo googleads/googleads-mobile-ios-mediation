6079846f751e6b34ca5259a65185532f11d542d6
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2025-03-17 09:42:00 -0700

    Updating DTExchange iOS adapter for release version 8.3.6.
    PiperOrigin-RevId: 737647238

--- adapters/DTExchange/DTExchangeAdapter/GADMAdapterFyberConstants.h
+++ adapters/DTExchange/DTExchangeAdapter/GADMAdapterFyberConstants.h
@@ -15,7 +15,7 @@
 #import <Foundation/Foundation.h>
 
 /// DT Exchange mediation adapter version.
-static NSString* const _Nonnull GADMAdapterFyberVersion = @"8.3.5.1";
+static NSString* const _Nonnull GADMAdapterFyberVersion = @"8.3.6.0";
 
 /// DT Exchange mediation adapter app ID key.
 static NSString* const _Nonnull GADMAdapterFyberApplicationID = @"applicationId";
