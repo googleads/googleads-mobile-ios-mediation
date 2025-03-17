711f9a6cd4149924eaa88c082c93a17af9e9ba1e
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2025-03-17 08:00:13 -0700

    Removed custom target property
    PiperOrigin-RevId: 737614977

--- AdapterUnitTests/AmazonAdapterTests/FakeApsClient.swift
+++ AdapterUnitTests/AmazonAdapterTests/FakeApsClient.swift
@@ -25,7 +25,6 @@ class FakeApsClient: NSObject, APSClient {
   static var triggerImpressionAfterAdLoad = false
   static var triggerAdClickAfterAdLoad = false
   static var showShouldSucceed = true
-  static var customTarget: [String: String]?
 
   static func resetTestFlags() {
     fetchShouldSucceed = true
@@ -35,7 +34,6 @@ class FakeApsClient: NSObject, APSClient {
     triggerImpressionAfterAdLoad = false
     triggerAdClickAfterAdLoad = false
     showShouldSucceed = true
-    customTarget = nil
   }
 
   func initialize(with appId: String, completion: @escaping (NSError?) -> Void) {
@@ -55,15 +53,8 @@ class FakeApsClient: NSObject, APSClient {
 
   func loadAndCacheApsAd(
     with slotId: String, clientAdFormat: APSClientAdFormat, adSize: CGSize,
-    customTarget: [String: String]?,
     completion: @escaping (AmazonBidLoadingAdapterRequestData?, NSError?) -> Void
   ) {
-
-    assert(
-      FakeApsClient.customTarget == customTarget,
-      "Inconsistent custom target. Expected: \(String(describing: FakeApsClient.customTarget)) but received: \(String(describing: customTarget))"
-    )
-
     if Self.signalsCollectionShouldSucceed {
       completion(
         AmazonBidLoadingAdapterRequestData(
