737cfdab8f0b38507b3fec8f584221a650036fe0
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2025-04-01 22:25:45 +0000

    Implemented banner, interstitial, and rewarded ads
    PiperOrigin-RevId: 742861681

---
+++ adapters/PubMaticAdapter/PubMaticAdapter/OpenWrapSDKClient.swift
@@ -0,0 +1,146 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import OpenWrapSDK
+import UIKit
+
+/// Factory that creates OpenWrapSDKClient.
+struct OpenWrapSDKClientFactory {
+
+  private init() {}
+
+  #if DEBUG
+    /// This property will be returned by |createClient| function if set in Debug mode.
+    nonisolated(unsafe) static var debugClient: OpenWrapSDKClient?
+  #endif
+
+  static func createClient() -> OpenWrapSDKClient {
+    #if DEBUG
+      return debugClient ?? OpenWrapSDKClientImpl()
+    #else
+      return OpenWrapSDKClientImpl()
+    #endif
+  }
+
+}
+
+/// A client for interacting with OpenWrapSDK.
+protocol OpenWrapSDKClient: NSObject {
+
+  /// Enable the OpenWrapSDK's COPPA configuration based on the provided boolean value. It enables if
+  /// the passed boolean value is true. Otherwise disable the COPPA configuration.
+  func enableCOPPA(_ enable: Bool)
+
+  /// Returns a version of OpenWrapSDK.
+  func version() -> String
+
+  /// Set up the OpenWrapSDK using the publisher ID and the profile IDs.
+  func setUp(
+    publisherId: String, profileIds: [NSNumber], completionHandler: @escaping ((any Error)?) -> Void
+  )
+
+  /// Collect signals for bidding.
+  func collectSignals(for adFormat: POBAdFormat) -> String
+
+  /// Load a banner ad.
+  @MainActor func loadRtbBannerView(bidResponse: String, delegate: POBBannerViewDelegate)
+
+  /// Load an interstitial ad.
+  func loadRtbInterstitial(bidResponse: String, delegate: POBInterstitialDelegate)
+
+  /// Load a rewarded ad.
+  func loadRtbRewardedAd(bidResponse: String, delegate: POBRewardedAdDelegate)
+
+  /// Present a POBInterstitial ad.
+  func presentInterstitial(from viewController: UIViewController) throws(PubMaticAdapterError)
+
+  /// Present a POBRewardedAd ad.
+  func presentRewardedAd(from viewController: UIViewController) throws(PubMaticAdapterError)
+
+}
+
+final class OpenWrapSDKClientImpl: NSObject, OpenWrapSDKClient {
+
+  var bannerView: POBBannerView?
+  var interstitial: POBInterstitial?
+  var rewardedAd: POBRewardedAd?
+
+  func version() -> String {
+    return OpenWrapSDK.version()
+  }
+
+  func setUp(
+    publisherId: String,
+    profileIds: [NSNumber],
+    completionHandler: @escaping ((any Error)?) -> Void
+  ) {
+    let config = OpenWrapSDKConfig(publisherId: publisherId, andProfileIds: profileIds)
+    OpenWrapSDK.initialize(with: config) { _, error in
+      completionHandler(error)
+    }
+  }
+
+  func enableCOPPA(_ enable: Bool) {
+    OpenWrapSDK.setCoppaEnabled(enable)
+  }
+
+  func collectSignals(for adFormat: POBAdFormat) -> String {
+    let config = POBSignalConfig(adFormat: adFormat)
+    // TODO(hikaruwatanabe): update gpid property.
+    config.gpid = nil
+    // TODO(hikaruwatanabe): update the host to admob from unknown once OpenWrapSDK supports it.
+    return POBSignalGenerator.generateSignal(for: .unknown, andConfig: config)
+  }
+
+  @MainActor
+  func loadRtbBannerView(bidResponse: String, delegate: POBBannerViewDelegate) {
+    bannerView = POBBannerView()
+    bannerView?.delegate = delegate
+    bannerView?.pauseAutoRefresh()
+    bannerView?.loadAd(withResponse: bidResponse)
+  }
+
+  func loadRtbInterstitial(
+    bidResponse: String,
+    delegate: any POBInterstitialDelegate
+  ) {
+    interstitial = POBInterstitial()
+    interstitial?.delegate = delegate
+    interstitial?.loadAd(withResponse: bidResponse)
+  }
+
+  func loadRtbRewardedAd(bidResponse: String, delegate: any POBRewardedAdDelegate) {
+    rewardedAd = POBRewardedAd()
+    rewardedAd?.delegate = delegate
+    rewardedAd?.load(withResponse: bidResponse)
+  }
+
+  func presentInterstitial(from viewController: UIViewController) throws(PubMaticAdapterError) {
+    guard let interstitial, interstitial.isReady else {
+      throw PubMaticAdapterError(
+        errorCode: .interstitialAdNotReadyForPresentation,
+        description: "Interstitial ad is not ready for presentation.")
+    }
+    interstitial.show(from: viewController)
+  }
+
+  func presentRewardedAd(from viewController: UIViewController) throws(PubMaticAdapterError) {
+    guard let rewardedAd, rewardedAd.isReady else {
+      throw PubMaticAdapterError(
+        errorCode: .rewardedAdNotReadyForPresentation,
+        description: "Rewarded ad is not ready for presentation.")
+    }
+    rewardedAd.show(from: viewController)
+  }
+}
---
+++ adapters/PubMaticAdapter/PubMaticAdapter/RewardedAdLoader.swift
@@ -0,0 +1,124 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import Foundation
+import GoogleMobileAds
+import OpenWrapSDK
+
+final class RewardedAdLoader: NSObject {
+
+  /// The rewarded ad configuration.
+  private let adConfiguration: MediationRewardedAdConfiguration
+
+  /// The ad event delegate which is used to report rewarded related information to the Google Mobile Ads SDK.
+  private weak var eventDelegate: MediationRewardedAdEventDelegate?
+
+  /// The completion handler that needs to be called upon finishing loading an ad.
+  private var rewardedAdLoadCompletionHandler: ((MediationRewardedAd?, NSError?) -> Void)?
+
+  /// The queue for processing an ad load completion.
+  private let adLoadCompletionQueue: DispatchQueue
+
+  /// The ad load completion handler the must be run after ad load completion.
+  private var adLoadCompletionHandler: GADMediationRewardedLoadCompletionHandler?
+
+  /// OpenWrapSDKClient used to manage a rewarded ad.
+  private let client: OpenWrapSDKClient
+
+  init(
+    adConfiguration: MediationRewardedAdConfiguration,
+    loadCompletionHandler: @escaping GADMediationRewardedLoadCompletionHandler
+  ) {
+    self.adConfiguration = adConfiguration
+    self.adLoadCompletionHandler = loadCompletionHandler
+    self.adLoadCompletionQueue = DispatchQueue(
+      label: "com.google.mediationRewardedAdLoadCompletionQueue")
+    self.client = OpenWrapSDKClientFactory.createClient()
+    super.init()
+  }
+
+  func loadAd() {
+    guard let bidResponse = adConfiguration.bidResponse else {
+      handleLoadedAd(
+        nil,
+        error: PubMaticAdapterError(
+          errorCode: .invalidAdConfiguration,
+          description: "The ad configuration is missing bid response."
+        ).toNSError())
+      return
+    }
+    client.loadRtbRewardedAd(bidResponse: bidResponse, delegate: self)
+  }
+
+  private func handleLoadedAd(_ ad: MediationRewardedAd?, error: Error?) {
+    adLoadCompletionQueue.sync {
+      guard let adLoadCompletionHandler else { return }
+      eventDelegate = adLoadCompletionHandler(ad, error)
+      self.adLoadCompletionHandler = nil
+    }
+  }
+
+}
+
+// MARK: - GADMediationRewardedAd
+
+extension RewardedAdLoader: MediationRewardedAd {
+
+  func present(from viewController: UIViewController) {
+    do {
+      try client.presentRewardedAd(from: viewController)
+    } catch let error {
+      eventDelegate?.didFailToPresentWithError(error.toNSError())
+    }
+  }
+
+}
+
+// MARK: - POBRewardedAdDelegate
+
+extension RewardedAdLoader: POBRewardedAdDelegate {
+
+  func rewardedAdDidReceive(_ rewardedAd: POBRewardedAd) {
+    handleLoadedAd(self, error: nil)
+  }
+
+  func rewardedAd(_ rewardedAd: POBRewardedAd, didFailToReceiveAdWithError error: any Error) {
+    handleLoadedAd(nil, error: error as NSError)
+  }
+
+  func rewardedAdWillPresent(_ rewardedAd: POBRewardedAd) {
+    eventDelegate?.willPresentFullScreenView()
+  }
+
+  func rewardedAd(_ rewardedAd: POBRewardedAd, didFailToShowAdWithError error: any Error) {
+    eventDelegate?.didFailToPresentWithError(error as NSError)
+  }
+
+  func rewardedAdDidRecordImpression(_ rewardedAd: POBRewardedAd) {
+    eventDelegate?.reportImpression()
+  }
+
+  func rewardedAdDidClick(_ rewardedAd: POBRewardedAd) {
+    eventDelegate?.reportClick()
+  }
+
+  func rewardedAdDidDismiss(_ rewardedAd: POBRewardedAd) {
+    eventDelegate?.didDismissFullScreenView()
+  }
+
+  func rewardedAd(_ rewardedAd: POBRewardedAd, shouldReward reward: POBReward) {
+    eventDelegate?.didRewardUser()
+  }
+
+}
