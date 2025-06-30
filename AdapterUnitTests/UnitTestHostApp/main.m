804f2ac211dc4991b77ef36a8ecf892e1ecac2d0
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2025-06-30 15:51:55 -0700

    Add a test app target in the AdapterUniTests.proj so that the tests can be run on real device.
    PiperOrigin-RevId: 777750362

---
+++ AdapterUnitTests/UnitTestHostApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UnitTestHostApp
+//
+//  Created by Hikaru Watanabe on 6/27/25.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  NSString * appDelegateClassName;
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+      appDelegateClassName = NSStringFromClass([AppDelegate class]);
+  }
+  return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
