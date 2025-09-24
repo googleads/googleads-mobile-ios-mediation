ea9954690513448889663d6136093d57b2be32a0
Jason Chiu <jasoncsh@google.com>
2025-09-24 10:27:31 -0700

    Internal changes.
    PiperOrigin-RevId: 810938831

---
+++ example/Snippets/Swift/AppLovinMediationSwiftSnippets.swift
@@ -0,0 +1,33 @@
+//
+//  Copyright (C) 2025 Google, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import AppLovinSDK
+import GoogleMobileAds
+
+/*
+ * Swift code snippets for
+ * https://developers.google.com/admob/ios/mediation/applovin and
+ * https://developers.google.com/ad-manager/mobile-ads-sdk/ios/mediation/applovin
+ */
+private class AppLovinMediationSwiftSnippets {
+
+  private func setUserConsent() {
+    // [START set_user_consent]
+    ALPrivacySettings.setHasUserConsent(true)
+    // [END set_user_consent]
+  }
+
+}
