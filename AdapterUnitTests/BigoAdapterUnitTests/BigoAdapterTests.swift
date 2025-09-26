e706944007275b2d4088bbbdc7f2fa221dd6ffe1
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2025-09-26 12:46:45 -0700

    Implemented version methods and added unit tests.
    PiperOrigin-RevId: 811921689

---
+++ AdapterUnitTests/BigoAdapterUnitTests/BigoAdapterTests.swift
@@ -0,0 +1,30 @@
+import AdapterUnitTestKit
+import XCTest
+
+@testable import BigoAdapter
+
+final class BigoAdapterTest: XCTestCase {
+
+  func testAdapterVersion() {
+    let version = BigoAdapter.adapterVersion()
+
+    XCTAssertGreaterThan(version.majorVersion, 0)
+    XCTAssertLessThanOrEqual(version.majorVersion, 99)
+    XCTAssertGreaterThanOrEqual(version.minorVersion, 0)
+    XCTAssertLessThanOrEqual(version.minorVersion, 99)
+    XCTAssertGreaterThanOrEqual(version.patchVersion, 0)
+    XCTAssertLessThanOrEqual(version.patchVersion, 9999)
+  }
+
+  func testAdSDKVersion() {
+    let version = BigoAdapter.adSDKVersion()
+
+    XCTAssertGreaterThan(version.majorVersion, 0)
+    XCTAssertLessThanOrEqual(version.majorVersion, 99)
+    XCTAssertGreaterThanOrEqual(version.minorVersion, 0)
+    XCTAssertLessThanOrEqual(version.minorVersion, 99)
+    XCTAssertGreaterThanOrEqual(version.patchVersion, 0)
+    XCTAssertLessThanOrEqual(version.patchVersion, 9999)
+  }
+
+}
