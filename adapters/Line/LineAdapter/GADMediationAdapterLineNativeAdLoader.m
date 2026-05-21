5686ca201dfe84f26d7b47259278a33183eb29cb
Mobile Ads Developer Relations <mad-devrel-noreply@google.com>
2026-05-21 09:21:57 -0700

    Update comment for information icon image resizing in LineAdapter.
    PiperOrigin-RevId: 919091149

--- adapters/Line/LineAdapter/GADMediationAdapterLineNativeAdLoader.m
+++ adapters/Line/LineAdapter/GADMediationAdapterLineNativeAdLoader.m
@@ -218,8 +218,10 @@ static NSUInteger GADMediationAdapterLineImageAssetLoadingTimeoutInSeconds = 10;
     if (informationIconImage) {
       GADMediationAdapterLineLog(@"Finished loading the information icon image.");
 
-      // Resize the image to an explicit target height of 20.0 points while scaling the width
-      // proportionally to accommodate information icons of varying aspect ratios or widths.
+      // The information icon image can be very large, so resize it to an explicit target height of
+      // 20.0 points while scaling the width proportionally to accommodate information icons of
+      // varying aspect ratios or widths. 20.0 points has been chosen because that is deemed to be
+      // an appropriate height for the icon image.
       CGFloat originalWidth = informationIconImage.size.width;
       CGFloat originalHeight = informationIconImage.size.height;
       CGFloat targetHeight = 20.0;
